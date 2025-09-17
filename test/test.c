ddbcb79b9a587ae746469de094c82960a2f2c3cc
jdaremes <samesjemerad@gmail.com>
2025-09-17 12:59:03 -0600

    Added some semaphore tests

--- test/test.c
+++ test/test.c
@@ -4,32 +4,87 @@
 #include <unity.h>
 #include "unity_config.h"
 
+// Semaphore libraries
+#include <FreeRTOS.h>
+#include <semphr.h>
+#include <task.h>
+
+SemaphoreHandle_t testaphore, twostaphore;
+int counter;
+
+#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+#define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+
+#define SIDE_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+#define SIDE_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+
 void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_variable_assignment()
+void test_semaphore_lock ()
 {
-    int x = 1;
-    TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
+    counter = 0;
+    xSemaphoreTake(testaphore, portMAX_DELAY);
+    counter++;
+
+    bool semaphoreBlock = xSemaphoreTake(testaphore, portMAX_DELAY);
+    counter--;
+
+    TEST_ASSERT_TRUE_MESSAGE(semaphoreBlock == false, "Semaphore creation incorrectly allowed (expected deadlock)");
 }
 
-void test_multiplication(void)
+void test_main_thread (void* pvParams) 
 {
-    int x = 30;
-    int y = 6;
-    int z = x / y;
-    TEST_ASSERT_TRUE_MESSAGE(z == 5, "Multiplication of two integers returned incorrect value.");
+    xSemaphoreTake(twostaphore, portMAX_DELAY);
+    if (counter < 100) {
+        printf("printing counter from %s, counter = %d", "main", counter);
+        counter++;
+    }
+
+    else
+        xSemaphoreGive(twostaphore); 
 }
+
+void test_side_thread (void* pvParams) 
+{
+    xSemaphoreTake(twostaphore, portMAX_DELAY);
+    if (counter < 100)
+        printf("printing counter from %s, counter = %d", "main", counter);    
+    else
+        xSemaphoreGive(twostaphore);
+}
+
+void test_threads_func () {
+    counter = 0;
+    TaskHandle_t main, side;
+    twostaphore = xSemaphoreCreateCounting(1, 1);
+
+    xTaskCreate(test_main_thread, "TestMainThread",
+                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &main);
+    xTaskCreate(test_side_thread, "TestSideThread",
+                SIDE_TASK_STACK_SIZE, NULL, SIDE_TASK_PRIORITY, &side);
+    //vTaskStartScheduler();
+}
+
 
 int main (void)
 {
     stdio_init_all();
-    sleep_ms(5000); // Give time for TTY to attach.
-    printf("Start tests\n");
-    UNITY_BEGIN();
-    RUN_TEST(test_variable_assignment);
-    RUN_TEST(test_multiplication);
     sleep_ms(5000);
-    return UNITY_END();
+
+    while (1) {
+        /*  uxMaxCount -- The maximum count that can be reached.
+            uxInitialCount -- The count value assigned to the semaphore when it is created.
+        */
+        testaphore = xSemaphoreCreateCounting(1, 1);
+    
+        printf("Start tests\n");
+        UNITY_BEGIN();
+        RUN_TEST(test_semaphore_lock);
+        RUN_TEST(test_threads_func);
+
+        sleep_ms(5000);
+        UNITY_END();
+    }
 }
