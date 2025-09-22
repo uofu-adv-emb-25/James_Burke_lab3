b9d6f67185ff183bbfd7d5fec940f4891fae6a95
BurkeDambly <burkeadambly@gmail.com>
2025-09-22 14:25:03 -0600

    Added a test to verify deadlocking, moved tests into a single task that is run on repeat

--- test/test.c
+++ test/test.c
@@ -24,14 +24,12 @@ void tearDown(void) {}
 
 void test_semaphore_lock ()
 {
-    counter = 0;
     xSemaphoreTake(testaphore, portMAX_DELAY);
-    counter++;
 
-    bool semaphoreBlock = xSemaphoreTake(testaphore, portMAX_DELAY);
-    counter--;
+    bool semaphoreBlock = xSemaphoreTake(testaphore, 100); // Try to take the semaphore again without waiting for too long
 
     TEST_ASSERT_TRUE_MESSAGE(semaphoreBlock == false, "Semaphore creation incorrectly allowed (expected deadlock)");
+    xSemaphoreGive(testaphore); 
 }
 
 void test_semaphore_give ()
@@ -47,14 +45,14 @@ void test_semaphore_give ()
 }
 
 
-void deadlock_one ( void *) {
+void deadlock_resource_one ( void *) {
     xSemaphoreTake(threestaphore, portMAX_DELAY);
     vTaskDelay(200); // Delay to create deadlock
 
-    xSemaphoreTake(semafour,      portMAX_DELAY);
+    xSemaphoreTake(semafour, portMAX_DELAY);
 }
 
-void deadlock_two ( void *) {
+void deadlock_resource_two ( void *) {
     xSemaphoreTake(semafour, portMAX_DELAY);
     vTaskDelay(200); // Delay to create deadlock
 
@@ -63,49 +61,57 @@ void deadlock_two ( void *) {
 
 
 void test_deadlock( void ) {
+
     TaskHandle_t dl_one, dl_two;
-
-    void * pvParameters = (&threestaphore, &semafour);
-
-    xTaskCreate(deadlock_one, "DEADLOCK_TEST", MAIN_TASK_STACK_SIZE, NULL, 
+    TaskStatus_t statusDetailOne, statusDetailTwo;
+    
+    xTaskCreate(deadlock_resource_one, "DEADLOCK_TEST", MAIN_TASK_STACK_SIZE, NULL, 
                             MAIN_TASK_PRIORITY, &dl_one);
 
-    xTaskCreate(deadlock_two, "DEADLOCK_TESTTWO", SIDE_TASK_STACK_SIZE, NULL,
+    xTaskCreate(deadlock_resource_two, "DEADLOCK_TESTTWO", SIDE_TASK_STACK_SIZE, NULL,
                             SIDE_TASK_PRIORITY, &dl_two);
 
     vTaskDelay(100);
-    vTaskSuspend(dl_one);
-    vTaskSuspend(dl_two);
 
-
-    TaskStatus_t statusDetailOne, statusDetailTwo;
-                    
+    
     vTaskGetInfo(dl_one, &statusDetailOne, pdTRUE, eInvalid);
-
     vTaskGetInfo(dl_two, &statusDetailTwo, pdTRUE, eInvalid);
-
+    
     eTaskState stateOne = statusDetailOne.eCurrentState;
     eTaskState stateTwo = statusDetailTwo.eCurrentState;
 
-    printf("Deadlock expected and Task one is currently: ");
-    printf("Deadlock expected and Task two is currently: ");
+    /*
+     typedef enum
+        {
+            eRunning = 0, A task is querying the state of itself, so must be running. 
+            eReady,       The task being queried is in a ready or pending ready list. 
+            eBlocked,     The task being queried is in the Blocked state. 
+            eSuspended,   The task being queried is in the Suspended state, or is in the Blocked state with an infinite time out. 
+            eDeleted,     The task being queried has been deleted, but its TCB has not yet been freed. 
+            eInvalid      Used as an 'invalid state' value.
+        } eTaskState;
+     */
 
-    TEST_ASSERT_TRUE_MESSAGE(true, "Error: reached end of expected code");
+
+    TEST_ASSERT_TRUE_MESSAGE((int)stateOne == 2, "Thread 1 gained access to resource 2.");
+    TEST_ASSERT_TRUE_MESSAGE((int)stateTwo == 2, "Thread 2 gained access to resource 1.");
+
+    vTaskSuspend(dl_one);
+    vTaskSuspend(dl_two);
+
+    vTaskDelete(dl_one);
+    vTaskDelete(dl_two);
+
 }
 
 
-
-int main (void)
-{
-    stdio_init_all();
-    sleep_ms(5000);
-
+void thread_testing(void *){
     while (1) {
         /*  uxMaxCount -- The maximum count that can be reached.
             uxInitialCount -- The count value assigned to the semaphore when it is created.
         */
         testaphore    = xSemaphoreCreateCounting(1, 1);
-        //twostaphore   = xSemaphoreCreateCounting(1, 1);
+        twostaphore   = xSemaphoreCreateCounting(1, 1);
         threestaphore = xSemaphoreCreateCounting(1, 1);
         semafour      = xSemaphoreCreateCounting(1, 1);
 
@@ -113,9 +119,20 @@ int main (void)
         UNITY_BEGIN();
         RUN_TEST(test_semaphore_lock);
         RUN_TEST(test_semaphore_give);
-
         RUN_TEST(test_deadlock);
         sleep_ms(5000);
         UNITY_END();
     }
+  
 }
+int main (void)
+{
+    stdio_init_all();
+    sleep_ms(5000);
+
+    xTaskCreate(thread_testing, "ThreadTesting",
+                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, NULL);
+
+    vTaskStartScheduler(); // This function should never return.
+}
+
